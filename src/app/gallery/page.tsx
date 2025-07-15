b680e99c7288dd3dd3a40091f11f8d98c4d1fae1
Gwendalynn Lim Wan Ting <gwenlynn.lim@gmail.com>
2025-07-15 07:17:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import * as React from "react"
 import Image from "next/image";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const graduatesImages = [
